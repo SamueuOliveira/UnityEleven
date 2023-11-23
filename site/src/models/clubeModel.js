83ef6a756893c798eb150346ab27cc8016ae2079
SamueuOliveira <samuel.batista@sptech.school>
2023-11-23 11:41:12 -0300

    login concluido

--- site/src/models/clubeModel.js
+++ site/src/models/clubeModel.js
@@ -1,5 +1,15 @@
 var database = require("../database/config");
 
+function cadastrarEquipe (email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM clube WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
 function listar() {
     var instrucao = `
         SELECT * FROM clube;
